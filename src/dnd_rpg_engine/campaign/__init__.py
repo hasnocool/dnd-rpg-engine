133f41540ef47076fdea9f51b89b78991278add2
@fleshwounded <101779643+hasnocool@users.noreply.github.com>
2026-08-16 19:09:10 -0700

    Add campaign orchestration package

---
+++ src/dnd_rpg_engine/campaign/__init__.py
@@ -0,0 +1,17 @@
+from dnd_rpg_engine.campaign.orchestrator import (
+    CampaignOrchestrator,
+    SceneDefinition,
+    SceneKind,
+    SceneRuntime,
+    SceneStatus,
+    SceneTransition,
+)
+
+__all__ = [
+    "CampaignOrchestrator",
+    "SceneDefinition",
+    "SceneKind",
+    "SceneRuntime",
+    "SceneStatus",
+    "SceneTransition",
+]
