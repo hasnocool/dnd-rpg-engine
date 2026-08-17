21096b77d934be8400433c5bfd5298f7bab2dd16
@fleshwounded <101779643+hasnocool@users.noreply.github.com>
2026-08-16 19:31:22 -0700

    Expose 2.5.0 package version

--- src/dnd_rpg_engine/__init__.py
+++ src/dnd_rpg_engine/__init__.py
@@ -1,7 +1,7 @@
-# src/dnd_rpg_engine/__init__.py
-"""Public package surface for the RPG engine."""
+"""Public package surface for the RPG platform."""
 
 from dnd_rpg_engine.characters.lifecycle import CharacterLifecycle, default_character_lifecycle
+from dnd_rpg_engine.client import RPGClient, RPGClientConfig
 from dnd_rpg_engine.core.advanced_engine import AdvancedGameEngine
 from dnd_rpg_engine.core.engine import GameEngine
 from dnd_rpg_engine.core.models import CampaignState, Entity, GameConfig, Stats, TimeMode
@@ -13,8 +13,10 @@ __all__ = [
     "Entity",
     "GameConfig",
     "GameEngine",
+    "RPGClient",
+    "RPGClientConfig",
     "Stats",
     "TimeMode",
     "default_character_lifecycle",
 ]
-__version__ = "1.8.0"
+__version__ = "2.5.0"
