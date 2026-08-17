340601e3afd4b4b3576b8430df371c660c89c59e
@fleshwounded <101779643+hasnocool@users.noreply.github.com>
2026-08-16 19:12:10 -0700

    Add distributed world package

---
+++ src/dnd_rpg_engine/distributed/__init__.py
@@ -0,0 +1,19 @@
+from dnd_rpg_engine.distributed.world import (
+    CrossShardMessage,
+    EntityTransfer,
+    ShardDirectory,
+    ShardStatus,
+    TransferCoordinator,
+    TransferStatus,
+    WorldShard,
+)
+
+__all__ = [
+    "CrossShardMessage",
+    "EntityTransfer",
+    "ShardDirectory",
+    "ShardStatus",
+    "TransferCoordinator",
+    "TransferStatus",
+    "WorldShard",
+]
