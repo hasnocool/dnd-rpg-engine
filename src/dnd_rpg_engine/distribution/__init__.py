1a903b130a58f892e8077119f74cb97d071f0648
@fleshwounded <101779643+hasnocool@users.noreply.github.com>
2026-08-16 19:11:36 -0700

    Add content distribution package

---
+++ src/dnd_rpg_engine/distribution/__init__.py
@@ -0,0 +1,19 @@
+from dnd_rpg_engine.distribution.packages import (
+    ContentDistributionIndex,
+    DependencyResolution,
+    HMACPackageSigner,
+    PackageRelease,
+    PackageSignature,
+    SemanticVersion,
+    VersionConstraint,
+)
+
+__all__ = [
+    "ContentDistributionIndex",
+    "DependencyResolution",
+    "HMACPackageSigner",
+    "PackageRelease",
+    "PackageSignature",
+    "SemanticVersion",
+    "VersionConstraint",
+]
