15459f9095fef6b9e34a1afae53876df8a95e7a6
@fleshwounded <101779643+hasnocool@users.noreply.github.com>
2026-08-16 18:31:30 -0700

    Export SRD character lifecycle adapter

--- src/dnd_rpg_engine/rulesets/srd_5_2_1/__init__.py
+++ src/dnd_rpg_engine/rulesets/srd_5_2_1/__init__.py
@@ -1,6 +1,7 @@
 # src/dnd_rpg_engine/rulesets/srd_5_2_1/__init__.py
 """Opt-in structured implementation of the CC-licensed SRD 5.2.1 foundation."""
 
+from dnd_rpg_engine.rulesets.srd_5_2_1.lifecycle import build_srd_character_lifecycle
 from dnd_rpg_engine.rulesets.srd_5_2_1.pack import build_srd_5_2_1_pack
 from dnd_rpg_engine.rulesets.srd_5_2_1.rules import (
     SRD_5_2_1_RULESET,
@@ -20,6 +21,7 @@ __all__ = [
     "SRD_5_2_1_RULESET",
     "ZeroHPTransition",
     "build_srd_5_2_1_pack",
+    "build_srd_character_lifecycle",
     "entity_proficiency_bonus",
     "fetch_official_srd_pdf",
     "proficiency_bonus",
