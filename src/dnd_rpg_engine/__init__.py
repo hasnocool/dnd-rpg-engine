2c224e0eefffb7feeda0072791058d62debe8244
@fleshwounded <101779643+hasnocool@users.noreply.github.com>
2026-08-16 18:40:38 -0700

    Expose v1.8 lifecycle platform surface

--- src/dnd_rpg_engine/__init__.py
+++ src/dnd_rpg_engine/__init__.py
@@ -1,10 +1,22 @@
 # src/dnd_rpg_engine/__init__.py
 """Public package surface for the RPG engine."""
 
+from dnd_rpg_engine.characters.lifecycle import CharacterLifecycle, default_character_lifecycle
 from dnd_rpg_engine.core.advanced_engine import AdvancedGameEngine
 from dnd_rpg_engine.core.engine import GameEngine
 from dnd_rpg_engine.core.models import CampaignState, Entity, GameConfig, Stats, TimeMode
 from dnd_rpg_engine.rulesets.srd_5_2_1 import SRDRuntimeCatalog
 
-__all__ = ["AdvancedGameEngine", "CampaignState", "Entity", "GameConfig", "GameEngine", "SRDRuntimeCatalog", "Stats", "TimeMode"]
-__version__ = "1.5.0"
+__all__ = [
+    "AdvancedGameEngine",
+    "CampaignState",
+    "CharacterLifecycle",
+    "Entity",
+    "GameConfig",
+    "GameEngine",
+    "SRDRuntimeCatalog",
+    "Stats",
+    "TimeMode",
+    "default_character_lifecycle",
+]
+__version__ = "1.8.0"
