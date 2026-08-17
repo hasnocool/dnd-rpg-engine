5a4087d6f5c11c4363f673d920e84f5dd64a482a
@fleshwounded <101779643+hasnocool@users.noreply.github.com>
2026-08-16 18:13:43 -0700

    Expose advanced integrated engine

--- src/dnd_rpg_engine/__init__.py
+++ src/dnd_rpg_engine/__init__.py
@@ -1,8 +1,9 @@
 # src/dnd_rpg_engine/__init__.py
 """Public package surface for the RPG engine."""
 
+from dnd_rpg_engine.core.advanced_engine import AdvancedGameEngine
 from dnd_rpg_engine.core.engine import GameEngine
 from dnd_rpg_engine.core.models import CampaignState, Entity, GameConfig, Stats, TimeMode
 
-__all__ = ["CampaignState", "Entity", "GameConfig", "GameEngine", "Stats", "TimeMode"]
+__all__ = ["AdvancedGameEngine", "CampaignState", "Entity", "GameConfig", "GameEngine", "Stats", "TimeMode"]
 __version__ = "1.5.0"
